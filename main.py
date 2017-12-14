45345bdbad3df9fe574c844b1152cb96d18f180a
Xavier Gomez <xendke@gmail.com>
2017-12-14 16:35:14 -0500

    creating canvas class wrapper

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ except ImportError:
     # Python3
     import tkinter as tk
 
+class Canvas(tk.Canvas):
+    def __init__(self, master, w=400, h=400):
+        super().__init__(master,width=w, height=h)
+        self.width = w
+        self.height = h
+        self.file_name = "canvas.ps"
+
+    def clear(self, event):
+        print("clearing")
+        self.create_rectangle(0,0,400,400, fill="white", outline="white", width=5);
+
+    def save(self, event):
+        print("saving")
+        self.postscript(file=self.file_name, colormode="gray")
+
+
 class Pen():
     def __init__(self, canvas):
         self.previous_x = 0
@@ -12,6 +28,7 @@ class Pen():
         self.canvas = canvas
         self.color = "black"
         self.width = 20
+        self.bind_actions()
 
     def set_previous(self, x, y):
         self.previous_x = x
@@ -27,39 +44,30 @@ class Pen():
     def draw(self, event):
         """ """
         offset = self.width/2
-        self.canvas.create_line(event.x, event.y, self.previous_x, self.previous_y, fill=self.color, width=self.width+self.width/5)
+        self.canvas.create_line(event.x, event.y, self.previous_x, self.previous_y, fill=self.color, width=self.width+1)
         self.canvas.create_oval(event.x-offset, event.y-offset, event.x+offset, event.y+offset, fill=self.color)
         # processing rounded corners mimic
         self.set_previous(event.x, event.y)
 
-def clear(event):
-    print("s")
-    cnv.create_rectangle(0,0,400,400, fill="white", outline="white", width=5);
-
-def save(event):
-    print("saving")
-    cnv.postscript(file="file_name.ps", colormode="gray")
-
+    def bind_actions(self):
+        cnv.bind("<Motion>", self.hovering)
+        cnv.bind("<FocusIn>", self.hovering)
+        cnv.bind("<B1-Motion>", self.draw)
+        # cnv.bind("<Button-1>", pn.draw)
+        cnv.bind("<Button-2>", self.canvas.save)
+        cnv.bind("<Leave>", self.canvas.clear)
 
 
 master = tk.Tk()
-
-cnv = tk.Canvas(master, width=400, height=400)
+cnv = Canvas(master)
 cnv.pack()
 pn = Pen(cnv)
 
-cnv.bind("<Motion>", pn.hovering)
-cnv.bind("<FocusIn>", pn.hovering)
-cnv.bind("<B1-Motion>", pn.draw)
-# cnv.bind("<Button-1>", pn.draw)
-cnv.bind("<Button-2>", save)
-cnv.bind("<Leave>", clear)
-
-
 while True:
     try:
-        x = master.winfo_pointerx()
-        y = master.winfo_pointery()
+        # x = master.winfo_pointerx()
+        # y = master.winfo_pointery()
+        pass
     except tk.TclError:
         print("done")
         break
