dc3a47eb7cbb1d9c5791e68c97d8232bfa4b8d54
Xavier Gomez <xendke@gmail.com>
2017-12-15 13:23:19 -0500

    commenting, banished python2

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-try:
-    # Python2
-    import Tkinter as tk
-except ImportError:
-    # Python3
-    import tkinter as tk
+import tkinter as tk
 
 class Canvas(tk.Canvas):
     def __init__(self, master, w=400, h=400):
@@ -17,6 +12,7 @@ class Canvas(tk.Canvas):
         self.create_rectangle(0,0,400,400, fill="white", outline="white", width=5);
 
     def save(self, event):
+        """ save screenshot of the canvas stored as postscript file """
         print("saving")
         self.postscript(file=self.file_name, colormode="gray")
 
@@ -31,39 +27,36 @@ class Pen():
         self.bind_actions()
 
     def set_previous(self, x, y):
+        """ store old mouse coordinates, used for drawing straight lines """
         self.previous_x = x
         self.previous_y = y
 
-    def getPencil(self):
-        return self
-
     def hovered(self, event):
-        print("hovr", event.x)
         self.set_previous(event.x, event.y)
 
     def draw(self, event):
-        """ """
+        """ draw a line for straight brush strokes and a circle for rounded corners """
         offset = self.width/2
         self.canvas.create_line(event.x, event.y, self.previous_x, self.previous_y, fill=self.color, width=self.width+1)
         self.canvas.create_oval(event.x-offset, event.y-offset, event.x+offset, event.y+offset, fill=self.color)
-        # processing rounded corners mimic
         self.set_previous(event.x, event.y)
 
     def bind_actions(self):
         cnv = self.canvas
-
         cnv.bind("<B1-Motion>", self.draw) # Left Click Drag
 
         cnv.bind("<Motion>", self.hovered) # Hover
         cnv.bind("<Button-1>", self.hovered) # Left Click
         cnv.bind("<Enter>", self.hovered) # Mouse Entered
 
+        # temporary
         cnv.bind("<Button-2>", cnv.save) # Right Click
         cnv.bind("<Leave>", cnv.clear) # Mouse Left
 
 def main():
     master = tk.Tk()
     master.title("digitaizer")
+    #master.iconbitmap('icon.ico')
     cnv = Canvas(master)
     cnv.pack()
     pn = Pen(cnv)
