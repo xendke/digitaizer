af9359ac16face9552fd5d196d35373a9e444607
Xavier Gomez <xendke@gmail.com>
2017-12-23 19:34:31 -0500

    start training function

--- network.py
+++ network.py
@@ -1,9 +1,11 @@
 import numpy as np # mathematical functions and vectors/matrices
 
 class Network():
-    def __init__(self, layer_sizes):
+    def __init__(self, layer_sizes, learning_config=[3.0, 10, 30]):
         self.layer_sizes = layer_sizes # an array of integers. eg: [3, 4, 2] 3 Input Neurons, 4 Hidden Neurons, 2 Output Neurons
         self.num_layers = len(layer_sizes)
+
+        self.learning_rate, self.mini_batch_size, self.epochs = learning_config # Network hyperparameters
 
         self.biases = [np.random.randn(y, 1) for y in layer_sizes[1:]] # TODO: find better random function to ease learning
         """ example representation of biases for layer_sizes = [3,4,2]
@@ -37,13 +39,25 @@ class Network():
         """
 
     def predict(self, io):
-        """ io is a numpy array of dimension (n,1) : [[x1], [x2], ..., [xn]]"""
+        """ feed input through network. io is a numpy array of dimension (self.layer_sizes[0],1) : [[x1], [x2], ...]"""
         for b, w in zip(self.biases, self.weights):
             io = sigmoid(np.dot(w,io)+b)
         return io
 
+    def fit(self, training_data):
+        size = len(training_data)
+        for i in range(0, self.epochs):
+            np.random.shuffle(training_data)
+            mini_batches = [training_data[j:j+self.mini_batch_size] for j in range(0, size, self.mini_batch_size)]
+            for mb in mini_batches:
+                # TODO:update w and b
+                pass
+            print("epoch end")
+        """ train the network """
+        return
+
 def sigmoid(v):
-    """ v is a Numpy array. the sigma function will be applied to each element in v. """
+    """ v is a Numpy array. the sigmoid function will be applied to each element in v. """
     return 1.0/(1.0+np.exp(-v))
 
 
