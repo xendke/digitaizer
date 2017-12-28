b85d8cd33047aabc7a7356d0cd79370d0a46f73f
Xavier Gomez <xendke@gmail.com>
2017-12-27 23:36:16 -0500

    add gradient descent inside fit function

--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+# network based on Michael Nielsen's http://neuralnetworksanddeeplearning.com/
 import numpy as np # mathematical functions and vectors/matrices
 
 class Network():
@@ -45,22 +46,31 @@ class Network():
         return io
 
     def fit(self, training_data):
+        """ train the network - training_data is set of tuples (x, y) where x is input(pixel data) and y is true output(label)"""
         size = len(training_data)
         for i in range(0, self.epochs):
             np.random.shuffle(training_data)
             mini_batches = [training_data[j:j+self.mini_batch_size] for j in range(0, size, self.mini_batch_size)]
             for mb in mini_batches:
-                # TODO:update w and b
-                pass
+                # gradients for C_x (cost function)
+                nabla_b = [np.zeros(b.shape) for b in self.biases]
+                nabla_w = [np.zeros(w.shape) for w in self.weights]
+                for x, y in mb: # apply gradient descent using backpropagation to each mini batch
+                    dnabla_b, dnabla_w = self.backprop(x, y) # TODO
+                    nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, dnabla_b)]
+                    nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, dnabla_w)]
+                self.weights = [w-(self.learning_rate/len(mb))*nw for w, nw in zip(self.weights, nabla_w)]
+                self.biases = [b-(self.learning_rate/len(mb))*nb for b, nb in zip(self.biases, nabla_b)]
             print("epoch end")
-        """ train the network """
         return
 
 def sigmoid(v):
     """ v is a Numpy array. the sigmoid function will be applied to each element in v. """
     return 1.0/(1.0+np.exp(-v))
 
+def sigmoid_prime(v):
+    """ derivative of the sigmoid function above."""
+    return sigmoid(v)*(1-sigmoid(v))
 
 if __name__ == '__main__':
-    net = Network([3,4,2])
-    print(net.predict([[0.00023],[0.22312],[0.763]]))
+    net = Network([784, 30, 10])
