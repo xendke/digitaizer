d28197db9f8d260454587d17c673fad1d32530b3
Xavier Gomez <xendke@gmail.com>
2018-01-16 00:23:54 -0500

    add network results to the GUI

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 import canvas # canvas wrapper widget
+import predictions_ui
 from network import Network
 import numpy as np
 
-def begin_prediction(net, canvas):
-    image_data = np.absolute(np.array(canvas.save())-255)/255
-    image_data = image_data[np.newaxis].T
+def begin_prediction(net, canvas, res):
+    image_data = np.absolute(np.array(canvas.grab())-255)/255 # reverse pixel value and transform to float {0..1}
+    image_data = image_data[np.newaxis].T # transpose
     # print(image_data)
-    prediction = np.argmax(net.predict(image_data))
-    print(prediction)
+    predictions = net.predict(image_data)
+    res.update(predictions)
 
 def main():
     net = Network([784, 30, 10])
@@ -19,13 +20,15 @@ def main():
 
     # using grid system to set the widgets in the window
     cnv = canvas.Canvas(master)
-    cnv.grid(row=0, column=0, columnspan=2)
+    cnv.grid(row=0, column=0, columnspan=2, rowspan=2)
+
+    res = predictions_ui.Results(master)
 
     bl = tk.Button(master, text="Clear Canvas", command=cnv.clear)
-    bl.grid(row=1, column=0)
+    bl.grid(row=2, column=0)
 
-    br = tk.Button(master, text="Predict It", command=lambda: begin_prediction(net, cnv))
-    br.grid(row=1, column=1)
+    br = tk.Button(master, text="Predict It", command=lambda: begin_prediction(net, cnv, res))
+    br.grid(row=2, column=1)
 
     master.mainloop() # this does the same as below and it runs the app constantly refreshes it
     # while True:
---
+++ predictions_ui.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import numpy as np
+
+class Results():
+    def __init__(self, master):
+        self.primary_text = tk.StringVar() # placeholders
+        self.primary_text.set("Prediction: N/A")
+        self.secondary_text = tk.StringVar()
+        self.secondary_text.set("Confidence: \n0 - N/A \n1 - N/A \n2 - N/A \n3 - N/A \n4 - N/A \n5 - N/A \n6 - N/A \n7 - N/A \n8 - N/A \n9 - N/A")
+
+
+        self.primary = tk.Message(master, textvariable=self.primary_text, width=200, justify='left', font=("Helvetica", 20))
+        self.primary.grid(row=0, column=2, rowspan=1)
+
+        self.secondary = tk.Message(master, textvariable=self.secondary_text, justify='left')
+        self.secondary.grid(row=1, column=2, rowspan=1)
+
+
+    def update(self, predictions):
+        """ replace placeholders with results from prediction"""
+        prediction = np.argmax(predictions) # max index of all predictions is the prediction
+        self.primary_text.set("Prediction: " + str(prediction))
+
+        t = "Confidence: \n"
+        for i in range(0,10):
+            t+=str(i) + " : " + str(round(predictions[i][0]*100, 4)) + "%" + "\n" # convert to percent and round
+            # the rounding will show some results to be 0% even though they are just really small floats
+        self.secondary_text.set(t)
