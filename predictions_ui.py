d4176c966da4d6b8929e1e41520ec14fc5334b0e
Xavier Gomez <xendke@gmail.com>
2018-01-25 12:16:07 -0500

    update formatting

--- predictions_ui.py
+++ predictions_ui.py
@@ -8,10 +8,10 @@ class Results():
         self.default_text()
 
         self.primary = tk.Message(master, textvariable=self.primary_text, width=200, justify='left', font=("Helvetica", 20))
-        self.primary.grid(row=0, column=2, rowspan=1)
+        self.primary.grid(row=0, column=2, rowspan=1, sticky=tk.N+tk.S)
 
         self.secondary = tk.Message(master, textvariable=self.secondary_text, justify='left')
-        self.secondary.grid(row=1, column=2, rowspan=1)
+        self.secondary.grid(row=1, column=2, rowspan=1, sticky=tk.W+tk.E+tk.N+tk.S)
 
     def default_text(self):
         """ set placeholder values to the label widgets """
@@ -20,8 +20,8 @@ class Results():
 
     def update(self, predictions):
         """ replace placeholders with results from prediction"""
-        predictions = predictions.T[0] # transpose
-        prediction = np.argmax(predictions) # get index of max value of all predictions is the network response
+        predictions = predictions.T[0]  # transpose
+        prediction = np.argmax(predictions)  # get index of max value of all predictions is the network response
         self.primary_text.set("Prediction: " + str(prediction))
 
         # construct dictionary where indexes are keys and the confidence results are values and then sort them as pairs
@@ -29,7 +29,12 @@ class Results():
         predictions_sorted = sorted(predictions_unsorted.items(), key=lambda x: x[1], reverse=True)
 
         t = "Confidence: \n"
-        for pair in predictions_sorted: # pair is tuple where pair[0] is label and pair[1] is value
-            t+=str(pair[0]) + " : " + str(round(pair[1]*100, 4)) + "%" + "\n" # convert to percent and round
+        i = 0
+        spacer = "\n\n"
+        for pair in predictions_sorted:  # pair is tuple where pair[0] is label and pair[1] is value
+            if(i==9):
+                spacer="\n"
+            t += str(pair[0]) + " : " + str(round(pair[1] * 100, 4)) + "%" + spacer  # convert to percent and round
             # the rounding will show some results to be 0% even though they are just really small floats
+            i+=1
         self.secondary_text.set(t)
